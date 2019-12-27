ae621bc300d216a3f4cf6de6f05d9bb16075a1c3
tyankatsu0105 <frips.ryilsufupe+dev@gmail.com>
2019-12-27 18:45:54 +0900

    feat: add commander

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,13 @@
-import { getPackageManager } from './lib';
+// import { getPackageManager } from './lib';
 
-console.log(getPackageManager(process.env.npm_config_user_agent));
+// console.log(getPackageManager(process.env.npm_config_user_agent));
+
+import program from 'commander';
+
+program
+  .name('install-with-types')
+  .usage('<pkg> [options]')
+  .option('-D, --save-dev', 'save dev')
+  .parse(process.argv);
+
+if (program['saveDev']) console.log('you use -D option');
